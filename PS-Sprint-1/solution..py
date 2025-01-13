aa51c520af7073503bd3cd97f07b30d5be005984
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-01-13 16:56:54 +0530

    sum of elements in an array

--- PS-Sprint-1/solution..py
+++ PS-Sprint-1/solution..py
@@ -1,11 +1,9 @@
-def avfOfArray(array):
-    total_sum = 0
-    avg = 0
+def sumOfElements(array):
+    sum = 0
     for i in range(len(array)):
-        total_sum += array[i]
-    avg = total_sum / len(array)
-    return avg
+        sum += array[i]
+    return sum
 
-array = [1, 2, 3, 4, 5, 6]
-result = avfOfArray(array)
+array = [1, 2, 3, 4, 5]
+result = sumOfElements(array)
 print(result)
